0358e7c18c260dd4a8f08a99bafda67edcdd5d8f
malmonte827git config --global user.name malmonte827 <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-03-05 10:13:32 -1000

    final version

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,23 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
 
+  it('should calculate the monthly rate correctly', function () {
+    // ...
+    const values = {amount: 100, years: 12, rate: 12};
+    expect(calculateMonthlyPayment(values)).toEqual('100.00');
+  });
+  
+  
+  it('should calculate the monthly to be >= 0', function () {
+    // ...
+    const values = {amount: 100, years: 12, rate: 12};
+  expect(calculateMonthlyPayment(values)).toBeGreaterThanOrEqual('0');
+});
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {amount: 100, years: 12, rate: 12};
+  expect(calculateMonthlyPayment(values)).toEqual('100.00')
+  
 });
 
 /// etc
