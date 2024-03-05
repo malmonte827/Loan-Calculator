ae914b1a20299abe4aafba0e2e9760076a56842b
malmonte827git config --global user.name malmonte827 <malmonte827@gmail.comgit config --global user.name malmonte827dgit configqgit config --global user.email malmonte827@gmail.com>
2024-03-05 08:59:30 -1000

    rough draft

--- calculator.js
+++ calculator.js
@@ -21,20 +21,43 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const defaultInputs = {amount: 30000, years: 5, rate: 5};
+  
+  const loanAmount = document.querySelector("#loan-amount");
+  loanAmount.value = defaultInputs.amount;
+
+  const loanYears = document.querySelector("#loan-years");
+  loanYears.value = defaultInputs.years; 
+  
+  const loanRate = document.querySelector("#loan-rate");
+  loanRate.value = defaultInputs.rate;
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+getCurrentUIValues()
+
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+const  monthlyRate = (values.rate / 12);
+const n = (values.years * 12);
+  return(
+(values.amount * monthlyRate)/ 1- (Math.pow((1 + monthlyRate), -n)).toFixed(2)
+);
+
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  monthlyPayment.innerText = "$" + monthly
 }
